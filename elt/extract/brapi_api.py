b55ad02c6e0b36541ce118f25d5912658fc58eec
Danton Bertuol <dantonjb@gmail.com>
2023-08-23 11:18:13 -0300

    fix: added param filter in function get_data to filter url and added doc

--- elt/extract/brapi_api.py
+++ elt/extract/brapi_api.py
@@ -1,4 +1,3 @@
-from src.api.api_consumer import Consumer
 from datetime import datetime
 import sys
 from pathlib import Path
@@ -8,16 +7,36 @@ PROJECT_PATH = Path(__file__).absolute().parent.parent.parent
 CHROME_DRIVER_PATH = f'{PROJECT_PATH}/bin/chromedriver/chromedriver'
 LOG_PATH = f'{PROJECT_PATH}/logs/elt_moodle/log_' + DATA_LOG + '.txt'
 
-sys.path.insert(1, str(PROJECT_PATH))
+sys.path.insert(1, str(PROJECT_PATH))  # insert path to run in windows
+from src.api.api_consumer import Consumer  # noqa: E402
 
 
 class BrapiAPI():
+    '''
+    Class to consume BrAPI API
+    '''
+
     def __init__(self, endpoint: str) -> None:
+        '''
+        Constructor
+
+        Args:
+            endpoint (str): endpoint to consume
+        '''
         self.endpoint = endpoint
         self.consumer = Consumer(self.endpoint)
 
-    def get_data(self) -> dict:
-        data: dict = self.consumer.get_data()
+    def get_data(self, filter: str = "") -> dict:
+        '''
+        Get data from endpoint
+
+        Args:
+            filter (str, optional): filter. Defaults to "".
+
+        Returns:
+            dict: data from endpoint
+        '''
+        data: dict = self.consumer.get_data(filter)
         return data
 
 
