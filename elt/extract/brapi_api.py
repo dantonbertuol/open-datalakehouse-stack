a60d542a46b54686e918dbd4090f2c9372579d48
Danton Bertuol <dantonjb@gmail.com>
2023-08-23 19:57:55 -0300

    fix: conflict

--- elt/extract/brapi_api.py
+++ elt/extract/brapi_api.py
@@ -4,44 +4,14 @@ from pathlib import Path
 
 DATA_LOG: str = datetime.now().strftime('%d-%m-%Y')
 PROJECT_PATH = Path(__file__).absolute().parent.parent.parent
-CHROME_DRIVER_PATH = f'{PROJECT_PATH}/bin/chromedriver/chromedriver'
-LOG_PATH = f'{PROJECT_PATH}/logs/elt_moodle/log_' + DATA_LOG + '.txt'
+LOG_PATH = f'{PROJECT_PATH}/logs/stocks_pipeline/log_' + DATA_LOG + '.txt'
 
-sys.path.insert(1, str(PROJECT_PATH))  # insert path to run in windows
+sys.path.insert(1, str(PROJECT_PATH))
 from src.api.api_consumer import Consumer  # noqa: E402
 
 
-class BrapiAPI():
+class BrapiAPI(Consumer):
     '''
-    Class to consume BrAPI API
+    Code to extract data from brapi.dev
+    inherits from Consumer class
     '''
-
-    def __init__(self, endpoint: str) -> None:
-        '''
-        Constructor
-
-        Args:
-            endpoint (str): endpoint to consume
-        '''
-        self.endpoint = endpoint
-        self.consumer = Consumer(self.endpoint)
-
-    def get_data(self, filter: str = "") -> dict:
-        '''
-        Get data from endpoint
-
-        Args:
-            filter (str, optional): filter. Defaults to "".
-
-        Returns:
-            dict: data from endpoint
-        '''
-        data: dict = self.consumer.get_data(filter)
-        return data
-
-
-if __name__ == '__main__':
-    endpoint: str = 'https://brapi.dev/api/available'
-    brapi_api = BrapiAPI(endpoint)
-    data: dict = brapi_api.get_data()
-    print(data)
