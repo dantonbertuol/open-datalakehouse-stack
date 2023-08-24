fa28a22f0aa7aac69ddea8f99299f07a717d9d8a
Danton Bertuol <dantonjb@gmail.com>
2023-08-24 16:53:11 -0300

    feat: created class to clean data from landing to processing zone

---
+++ elt/transform/clean_data.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import sys
+from pathlib import Path
+
+DATA_LOG: str = datetime.now().strftime('%d-%m-%Y')
+PROJECT_PATH = Path(__file__).absolute().parent.parent.parent
+LOG_PATH = f'{PROJECT_PATH}/logs/stocks_pipeline/log_' + DATA_LOG + '.txt'
+
+sys.path.insert(1, str(PROJECT_PATH))
+from src.datalake.s3connect import S3Connect  # noqa: E402
+
+
+class CleanData():
+    def __init__(self, env: str) -> None:
+        self.s3 = S3Connect(env)
+
+    def clean_table(self, path: str, bucket_from: str, bucket_to: str, fields: list):
+        try:
+            df = self.s3.get_data(bucket_from, path, 'csv')
+
+            clean_df = df.select(fields)
+
+            self.s3.insert_data(clean_df, bucket_to, path, 'overwrite', 'parquet')
+        except Exception as e:
+            print(f"Error to clean table {path}: {e}")
+
+    def close_s3_connection(self):
+        self.s3.close_spark_session()
+
+
+if __name__ == '__main__':
+    clean_data = CleanData('TESTE')
+
+    fields = ['symbol']
+    clean_data.clean_table('stocks/stock', 'landing', 'processing', fields)
+
+    fields = ['symbol', 'longName', 'shortName', 'currency', 'marketCap',
+              'regularMarketPrice', 'regularMarketVolume', 'regularMarketTime']
+    clean_data.clean_table('stocks/stock_quotes', 'landing', 'processing', fields)
+
+    clean_data.close_s3_connection()
