2862d380e905ee899c77bdc6bf853b70aa769bb8
Danton Bertuol <dantonjb@gmail.com>
2023-09-26 10:40:36 -0300

    refactor: separada classe de extração da classe do pipeline and added logs info

---
+++ pipelines/extract_data.py
@@ -0,0 +1,240 @@
+from datetime import datetime
+import sys
+from pathlib import Path
+
+DATA_LOG: str = datetime.now().strftime('%d-%m-%Y')
+PROJECT_PATH = Path(__file__).absolute().parent.parent
+LOG_PATH = f'{PROJECT_PATH}/logs/stocks_pipeline/log_' + DATA_LOG + '.txt'
+
+sys.path.insert(1, str(PROJECT_PATH))  # insert path to run in windows
+from elt.extract.brapi_api import BrapiAPI  # noqa: E402
+from elt.extract.yfinance_indicators import Indicators  # noqa: E402
+from src.utils.logs import Logs  # noqa: E402
+from src.database.mysql import MySQL  # noqa: E402
+
+
+class ExtractData():
+    '''
+    Class to extract stocks data
+    '''
+
+    def __init__(self, env: str) -> None:
+        '''
+        Constructor method
+        '''
+        self.logs = Logs(LOG_PATH)
+        self.env = env
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Starting Extract Data in {self.env}')
+
+    def create_tables_struct(self) -> None:
+        '''
+        Create table stock structure
+        '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Creating tables structure')
+
+        database = MySQL(
+            host='localhost',
+            user='root',
+            password='BrapiDev',
+            database='stock_quotes'
+        )
+
+        result: list = [True, '']
+
+        if not database.verify_table_exists('stock'):
+            result = database.create_table('sql/create_tables/stock.sql')
+            if result[0]:
+                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock created')
+            else:
+                self.logs.write(
+                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock not created. Error: {result[1]}')
+
+        if not database.verify_table_exists('stock_quotes'):
+            result = database.create_table('sql/create_tables/stock_quotes.sql')
+            if result[0]:
+                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_quotes created')
+            else:
+                self.logs.write(
+                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_quotes not created.'
+                    f' Error: {result[1]}')
+
+        if not database.verify_table_exists('stock_indicators'):
+            result = database.create_table('sql/create_tables/stock_indicators.sql')
+            if result[0]:
+                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_indicators created')
+            else:
+                self.logs.write(
+                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_indicators not created.'
+                    f' Error: {result[1]}')
+
+        if not database.verify_table_exists('stock_dividends'):
+            result = database.create_table('sql/create_tables/stock_dividends.sql')
+            if result[0]:
+                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_dividends created')
+            else:
+                self.logs.write(
+                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_dividends not created.'
+                    f' Error: {result[1]}')
+
+        database.close_connection()
+
+    def extract_api_data(self, endpoint: str) -> None:
+        '''
+        Extract data from api
+
+        Args:
+            endpoint (str): endpoint url
+
+        Returns:
+            dict: dict data
+        '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Consuming endpoint {endpoint}')
+
+        data: dict = {}
+        result_: list = [True, '']
+        database = MySQL(
+            host='localhost',
+            user='root',
+            password='BrapiDev',
+            database='stock_quotes'
+        )
+
+        # Available Endpoint
+        if 'available' in endpoint:
+            brapi_api = BrapiAPI(endpoint)
+            data = brapi_api.get_data()
+            if data.get('error') is None:
+                stocks = data.get('stocks')
+
+                for stock in stocks:  # type: ignore
+                    # Não insere ações fracionarias
+                    if stock.endswith('F'):
+                        continue
+
+                    result_ = database.insert_data('stock', {'symbol': stock})
+                    if not result_[0]:
+                        self.logs.write(
+                            f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Stock {stock} not inserted.'
+                            f' Error: {result_[1]}')
+            else:
+                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
+                                f'Error consuming endpoint {endpoint}')
+
+        # Quote Endpoint
+        elif 'quote' in endpoint:
+            stocks = database.get_data('stock', 'symbol')
+            stocks = [stock[0] for stock in stocks]
+
+            # Somente busca cotação se retornou algum stock
+            if len(stocks) > 0:
+                quotes = BrapiAPI(endpoint)
+
+                # Busca 200 por vez por limitação da API
+                for stock in stocks:
+                    quotes_data: dict = quotes.get_data(stock)
+
+                    if quotes_data.get('error') is None:
+                        for result in quotes_data.get('results'):  # type: ignore
+                            if result.get('error'):
+                                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
+                                                f'Error stock quote {result.get("symbol")} > {result.get("message")}')
+                                result_ = [False, '']
+                                continue
+
+                            result_ = database.insert_data('stock_quotes', result)
+                            if not result_[0]:
+                                self.logs.write(
+                                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
+                                    f'Stock Quote {result.get("symbol")} not inserted. Error: {result_[1]}')
+                    else:
+                        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
+                                        f'Error consuming endpoint {endpoint + stock} -> {quotes_data.get("error")}')
+            else:
+                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: No stock to search')
+
+        database.close_connection()
+
+    def extract_indicators(self):
+        '''
+        Extract indicators from yfinance api
+        '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Extracting indicators')
+
+        result: list = [True, '']
+        indicators = Indicators()
+
+        database = MySQL(
+            host='localhost',
+            user='root',
+            password='BrapiDev',
+            database='stock_quotes'
+        )
+
+        result = database.truncate_table('stock_indicators')
+
+        if result[0]:
+            stocks = database.get_data('stock', 'symbol')
+
+            stocks = [stock[0] + '.SA' for stock in stocks]
+
+            stock_indicators = indicators.get_indicators(' '.join(stocks))
+
+            for stock in stock_indicators.keys():
+                result = database.insert_data('stock_indicators', stock_indicators[stock])
+                if not result[0]:
+                    self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
+                                    f'Stock Indicator {stock} not inserted. Error: {result[1]}')
+        else:
+            self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
+                            f'Error truncating table stock_indicators. Error: {result[1]}')
+
+        database.close_connection()
+
+    def extract_dividends(self):
+        '''
+        Extract dividends from yfinance api
+        '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Extracting dividends')
+
+        indicators = Indicators()
+        data: dict = {}
+        result: list = [True, '']
+
+        database = MySQL(
+            host='localhost',
+            user='root',
+            password='BrapiDev',
+            database='stock_quotes'
+        )
+
+        stocks = database.get_data('stock', 'symbol')
+
+        stocks = [stock[0] + '.SA' for stock in stocks]
+
+        stock_indicators = indicators.get_dividends(' '.join(stocks))
+
+        for stock in stock_indicators.keys():
+            for date, value in zip(stock_indicators[stock]['paymentDate'], stock_indicators[stock]['amount']):
+                data = {
+                    'symbol': stock,
+                    'paymentDate': date,
+                    'amount': value
+                }
+                result = database.insert_data('stock_dividends', data)
+                if not result[0]:
+                    self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
+                                    f'Stock Dividend {stock} not inserted. Error: {result[1]}')
+
+        database.close_connection()
+
+
+if __name__ == '__main__':
+    extract_data = ExtractData('TESTE')
+
+    extract_data.create_tables_struct()
+
+    extract_data.extract_api_data('https://brapi.dev/api/available/')
+    extract_data.extract_api_data('https://brapi.dev/api/quote/')
+
+    extract_data.extract_indicators()
+    extract_data.extract_dividends()
--- pipelines/stock_quotes.py
+++ pipelines/stock_quotes.py
@@ -9,10 +9,7 @@ BUCKET_DATALAKE = 'datalake'
 BUCKET_LAKEHOUSE = 'lakehouse'
 
 sys.path.insert(1, str(PROJECT_PATH))  # insert path to run in windows
-from elt.extract.brapi_api import BrapiAPI  # noqa: E402
-from elt.extract.yfinance_indicators import Indicators  # noqa: E402
 from src.utils.logs import Logs  # noqa: E402
-from src.database.mysql import MySQL  # noqa: E402
 from elt.transform.clean_data import CleanData  # noqa: E402
 from elt.transform.convert_to_delta import ConvertDeltaTables  # noqa: E402
 from elt.transform.enrich_delta import EnrichDelta  # noqa: E402
@@ -30,187 +27,7 @@ class StockQuotesPipeline(Logs):
         '''
         self.logs = Logs(LOG_PATH)
         self.env = env
-
-    def create_tables_struct(self) -> None:
-        '''
-        Create table stock structure
-        '''
-        database = MySQL(
-            host='localhost',
-            user='root',
-            password='BrapiDev',
-            database='stock_quotes'
-        )
-
-        result: list = [True, '']
-
-        if not database.verify_table_exists('stock'):
-            result = database.create_table('sql/create_tables/stock.sql')
-            if result[0]:
-                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock created')
-            else:
-                self.logs.write(
-                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock not created. Error: {result[1]}')
-
-        if not database.verify_table_exists('stock_quotes'):
-            result = database.create_table('sql/create_tables/stock_quotes.sql')
-            if result[0]:
-                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_quotes created')
-            else:
-                self.logs.write(
-                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_quotes not created.'
-                    f' Error: {result[1]}')
-
-        if not database.verify_table_exists('stock_indicators'):
-            result = database.create_table('sql/create_tables/stock_indicators.sql')
-            if result[0]:
-                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_indicators created')
-            else:
-                self.logs.write(
-                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_indicators not created.'
-                    f' Error: {result[1]}')
-
-        if not database.verify_table_exists('stock_dividends'):
-            result = database.create_table('sql/create_tables/stock_dividends.sql')
-            if result[0]:
-                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_dividends created')
-            else:
-                self.logs.write(
-                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Table stock_dividends not created.'
-                    f' Error: {result[1]}')
-
-        database.close_connection()
-
-    def extract_api_data(self, endpoint: str) -> None:
-        '''
-        Extract data from api
-
-        Args:
-            endpoint (str): endpoint url
-
-        Returns:
-            dict: dict data
-        '''
-        data: dict = {}
-        result_: list = [True, '']
-        database = MySQL(
-            host='localhost',
-            user='root',
-            password='BrapiDev',
-            database='stock_quotes'
-        )
-
-        # Available Endpoint
-        if 'available' in endpoint:
-            brapi_api = BrapiAPI(endpoint)
-            data = brapi_api.get_data()
-            if data.get('error') is None:
-                stocks = data.get('stocks')
-
-                for stock in stocks:  # type: ignore
-                    # Não insere ações fracionarias
-                    if stock.endswith('F'):
-                        continue
-
-                    result_ = database.insert_data('stock', {'symbol': stock})
-                    if not result_[0]:
-                        self.logs.write(
-                            f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Stock {stock} not inserted.'
-                            f' Error: {result_[1]}')
-            else:
-                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
-                                f'Error consuming endpoint {endpoint}')
-
-        # Quote Endpoint
-        elif 'quote' in endpoint:
-            stocks = database.get_data('stock', 'symbol')
-            stocks = [stock[0] for stock in stocks]
-
-            # Somente busca cotação se retornou algum stock
-            if len(stocks) > 0:
-                quotes = BrapiAPI(endpoint)
-
-                # Busca 200 por vez por limitação da API
-                for i in range(0, len(stocks), 200):
-                    quotes_data: dict = quotes.get_data(",".join(stocks[i:i+200]))
-
-                    if quotes_data.get('error') is None:
-                        for result in quotes_data.get('results'):  # type: ignore
-                            if result.get('error'):
-                                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
-                                                f'Error stock quote {result.get("symbol")} > {result.get("message")}')
-                                result_ = [False, '']
-                                continue
-
-                            result_ = database.insert_data('stock_quotes', result)
-                            if not result_[0]:
-                                self.logs.write(
-                                    f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
-                                    f'Stock Quote {result.get("symbol")} not inserted. Error: {result_[1]}')
-                    else:
-                        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
-                                        f'Error consuming endpoint {endpoint} -> {quotes_data.get("error")}')
-            else:
-                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: No stock to search')
-
-        database.close_connection()
-
-    def extract_indicators(self):
-        '''
-        Extract indicators from yfinance api
-        '''
-        indicators = Indicators()
-
-        database = MySQL(
-            host='localhost',
-            user='root',
-            password='BrapiDev',
-            database='stock_quotes'
-        )
-
-        stocks = database.get_data('stock', 'symbol')
-
-        stocks = [stock[0] + '.SA' for stock in stocks]
-
-        stock_indicators = indicators.get_indicators(' '.join(stocks))
-
-        for stock in stock_indicators.keys():
-            result = database.insert_data('stock_indicators', stock_indicators[stock])
-            if not result[0]:
-                self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
-                                f'Stock Indicator {stock} not inserted. Error: {result[1]}')
-
-    def extract_dividends(self):
-        '''
-        Extract dividends from yfinance api
-        '''
-        indicators = Indicators()
-        data: dict = {}
-
-        database = MySQL(
-            host='localhost',
-            user='root',
-            password='BrapiDev',
-            database='stock_quotes'
-        )
-
-        stocks = database.get_data('stock', 'symbol')
-
-        stocks = [stock[0] + '.SA' for stock in stocks]
-
-        stock_indicators = indicators.get_dividends(' '.join(stocks))
-
-        for stock in stock_indicators.keys():
-            for date, value in zip(stock_indicators[stock]['paymentDate'], stock_indicators[stock]['amount']):
-                data = {
-                    'symbol': stock,
-                    'paymentDate': date,
-                    'amount': value
-                }
-                result = database.insert_data('stock_dividends', data)
-                if not result[0]:
-                    self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: '
-                                    f'Stock Dividend {stock} not inserted. Error: {result[1]}')
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Starting Stock Quotes Pipeline in {self.env}')
 
     def clean_data(self, path: list, bucket_from: list, bucket_to: list, fields: list) -> None:
         '''
@@ -223,6 +40,8 @@ class StockQuotesPipeline(Logs):
             bucket_to (list): list of buckets to write data cleaned
             fields (list): list of fields to select
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Cleaning data')
+
         result: list = [True, '']
 
         clean_data = CleanData(self.env)
@@ -245,6 +64,8 @@ class StockQuotesPipeline(Logs):
             bucket_from (str): bucket from get data to convert
             bucket_to (str): bucket to write data converted
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Converting to delta')
+
         result: list = [True, '']
 
         convert_delta = ConvertDeltaTables(self.env)
@@ -263,6 +84,8 @@ class StockQuotesPipeline(Logs):
         '''
         Method to enrich delta table
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Enriching table stock_quotes')
+
         result: list = [True, '']
 
         enrich_delta = EnrichDelta(self.env)
@@ -285,6 +108,8 @@ class StockQuotesPipeline(Logs):
         '''
         Method to enrich delta table
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Enriching table stock_indicators')
+
         result: list = [True, '']
 
         enrich_delta = EnrichDelta(self.env)
@@ -306,6 +131,8 @@ class StockQuotesPipeline(Logs):
         '''
         Method to enrich delta table
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Enriching table stock_dividends')
+
         result: list = [True, '']
 
         enrich_delta = EnrichDelta(self.env)
@@ -327,6 +154,8 @@ class StockQuotesPipeline(Logs):
         '''
         Method to gold stock table
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Creating Gold table stock')
+
         result: list = [True, '']
 
         gold_tables = GoldTables(self.env)
@@ -349,6 +178,8 @@ class StockQuotesPipeline(Logs):
         '''
         Method to gold stock quotes table
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Creating Gold table stock_quotes')
+
         result: list = [True, '']
 
         gold_tables = GoldTables(self.env)
@@ -370,6 +201,8 @@ class StockQuotesPipeline(Logs):
         '''
         Method to gold stock indicators table
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Creating Gold table stock_indicators')
+
         result: list = [True, '']
 
         gold_tables = GoldTables(self.env)
@@ -391,6 +224,8 @@ class StockQuotesPipeline(Logs):
         '''
         Method to gold stock dividends table
         '''
+        self.logs.write(f'{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}: Creating Gold table stock_dividends')
+
         result: list = [True, '']
 
         gold_tables = GoldTables(self.env)
@@ -411,13 +246,6 @@ class StockQuotesPipeline(Logs):
 
 if __name__ == '__main__':
     pipeline = StockQuotesPipeline('TESTE')
-
-    pipeline.create_tables_struct()
-    pipeline.extract_api_data('https://brapi.dev/api/available/')
-    pipeline.extract_api_data('https://brapi.dev/api/quote/')
-
-    pipeline.extract_indicators()
-    pipeline.extract_dividends()
 
     # tables_to_clean = ['stocks/stock', 'stocks/stock_quotes']
     # buckets_from = ['landing', 'landing']
